8e5d4fdc4cfdd24054dee2b3bdb97c6d89fff35a
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2022-02-08 23:18:44 +0300

    add home page, accessed after log in

---
+++ home.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Hello,  <?php echo $_SESSION['name']; ?></h1>
+    <button>
+    <a href="logout.php">Logout</a>
+
+    </button>
+    
+</body>
+</html>
+
+<?php
+}else {
+    header("Location: index.php");
+    exit();
+
+}
+?>
