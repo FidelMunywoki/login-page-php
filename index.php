f44681e956ff78376d79f337518f54c9b871a867
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2022-02-08 23:15:52 +0300

    update

--- index.php
+++ index.php
@@ -11,6 +11,14 @@
 
 <form action="login.php" method="post">
 <h2>LOGIN</h2>
+
+     <?php 
+     if (isset($_GET['error'])) { ?>
+     <p class="error"> <?php echo $_GET['error']; ?></p>
+     
+     <?php } ?>
+
+     
     <label for="">User Name</label>
     <input type="text" name="uname" placeholder = "User Name"> <br >
 
