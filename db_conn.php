1e94c409e2903c058c5698954b609981dfd0e90f
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2022-03-15 22:19:02 +0300

    update db connect

--- db_conn.php
+++ db_conn.php
@@ -1,10 +1,14 @@
 <?php
 
-$sname = "localhost";
-$db_login_name = "root";
-$pass = "xxxx";
+// $sname = "localhost";
+$sname = "us-cdbr-east-05.cleardb.net";
+// $db_login_name = "root";
+$db_login_name = "b85d5ab4562a4c";
+//$pass = " ";
+$pass = "d34ae1a4";
 
-$db_name = "test_db";
+//$db_name = "test_db";
+$db_name = "heroku_a9ff77dede75a46";
 
 $conn = mysqli_connect($sname,$db_login_name,$pass,$db_name);
 
